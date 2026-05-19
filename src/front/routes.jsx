4e4456f72dbd62d95dbba06fbf1d771ed10b80f9
Ana Lopez <avantadesignlab@gmail.com>
2026-05-19 23:36:02 +0000

    feat:create Signup view, endpoint and route

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,8 +7,10 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+import { Signup } from "./pages/Signup";
+// import { Signup } from "./pages/Signup";
+// import { Single } from "./pages/Single";
+// import { Demo } from "./pages/Demo";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,8 +25,9 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
+        <Route path= "/" element={<Signup />} />
+        {/* <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
+        {/* <Route path="/demo" element={<Demo />} /> */} */}
       </Route>
     )
 );
